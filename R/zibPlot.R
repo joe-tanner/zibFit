2fdaf3586d5d46d0c87d15651adc97b9e82ac6aa
joe-tanner <joe.tanner.2026@mumail.ie>
2026-01-08 17:12:58 +0000

    Initial commits for zibFit and zibPlot functions

---
+++ R/zibPlot.R
@@ -0,0 +1,34 @@
+# method for plot() that plots the distribution
+
+plot.zibSim <- function(x, microbe_name = "Microbe", ...) {
+
+  # get the data
+  df <- x$y
+
+  # create the plot
+  # par(new = TRUE)
+  breaks <- seq(0, 1, by = 0.025)
+  non_zero <- subset(df, df != 0)
+
+  fit <- MASS::fitdistr(non_zero, dbeta, start = list(shape1 = 1, shape2 = 1))
+
+  zero_prop <- mean(df == 0)
+  par(mar = c(5, 4, 4, 5) + 0.1)
+  hist(df, freq = FALSE, xlim = c(0, 1), breaks = breaks,
+       main = microbe_name, xlab = "Abundance", ylab = "Density")
+  curve(dbeta(x, fit$estimate["shape1"], fit$estimate["shape2"]),
+        col = "blue", lwd = 2, add = TRUE)
+  par(col.axis = "blue", col.lab = "blue")
+  par(new = TRUE, col.axis = "red")
+  plot(0, 0, type = "n", axes = FALSE, xlab = "", ylab = "",
+       xlim = range(df), ylim = c(0, 1))
+  lines(c(0, 0), c(0, zero_prop), col = "red", lwd = 3)
+  axis(side = 4, at = seq(0, 1, 0.2))
+  mtext("Zero proportion", side = 4, line = 3, col = "red")
+  leg.txt = c("Non-Zero Density", "Zero Proportion")
+  leg.col = c("blue", "red")
+  legend("topright", legend = leg.txt, text.col = leg.col, bty = "n")
+
+}
+
+
---
+++ R/zibSim.R
@@ -0,0 +1,80 @@
+zibSim <- function(n, t, a, b, sigma1, sigma2, phi, alpha, beta, X, Z, seed = NULL) {
+
+  # n = number of samples to simulate
+  # t = number of time points to simulate
+  # a = random effect mean (probability of being 0)
+  # b = random effect mean (average of beta distribution)
+  # sigma1 = random effect variance (probability of being 0)
+  # sigma2 = random effect variance (average of beta distribution)
+  # phi = beta dispersion parameter
+  # alpha = regression coefficients (same length as the number number of columns in X)
+  # beta = regression coefficients (same length as the number number of columns in Z)
+  # X = covariate matrix (logistic) (must include 'Subject' and 'Time' columns)
+  # Z = covariate matrix (beta) (must include 'Subject' and 'Time' columns)
+  # seed = simulation seed
+
+  # check inputs
+  if(length(n) != 1 | length(t) != 1) stop("n, t must be integers of length 1")
+  if(!is.numeric(n)) stop("n must be an integer of length 1")
+  if(!is.numeric(t)) stop("t must be an integer of length 1")
+
+  if(length(a) != 1 | length(b) != 1) stop("a, b must be integers of length 1")
+  if(!is.numeric(a)) stop("a must be an integer of length 1")
+  if(!is.numeric(b)) stop("b must be an integer of length 1")
+
+  if(length(sigma1) != 1 | length(sigma2) != 1) stop("sigma 1, 2 must be integers of length 1")
+  if(!is.numeric(sigma1)) stop("sigma 1 must be an integer of length 1")
+  if(!is.numeric(sigma2)) stop("sigma 2 must be an integer of length 1")
+
+  if(length(phi) != 1) stop("phi must be an integer of length 1")
+  if(!is.numeric(phi)) stop("phi must be an integer of length 1")
+
+  if(length(alpha) != ncol(X)) stop("alpha must have the same number of integers as X has columns")
+  if(length(beta) != ncol(Z)) stop("beta must have the same number of integers as Z has columns")
+
+  if(is.null(seed)) seed = round(a*b*n*t*phi, 0)
+
+  set.seed(seed)
+  results = rep(0, times = n)
+
+  # sample random effects from normal distributions
+  sample_a <- rnorm(n = n, mean = a, sd = sqrt(sigma1))
+  sample_b <- rnorm(n = n, mean = b, sd = sqrt(sigma2))
+
+  for(i in 1:n) {
+    for(j in 1:t) {
+      m = (i-1)*t + j # works provided it's ordered correctly - look at??
+
+      inv_logit <- function(x) exp(x)/(1 + exp(x))
+      p = inv_logit(sample_a[i] + (X[m, ]%*%alpha))
+
+      if(runif(1) < p) {
+        u = inv_logit(sample_b[i] + (Z[m,]%*%beta))
+        y = rbeta(n = 1, shape1 = u*phi, shape2 = (1-u)*phi)
+      } else {
+        y = 0
+      }
+
+      # catch all just in case numbers are going to be rounded to 1
+      if (y > 1 - 10^(-6)) {
+        y <- 1 - 10^(-6)
+      }
+
+      results[m] = y
+
+    }
+  }
+
+  subject_ind = rep(1:n, each = t)
+  time_ind = rep(1:t, n)
+  parameters = list(a = a, b = b, sigma1 = sigma1, sigma2 = sigma2, phi = phi,
+                    alpha = alpha, beta = beta)
+  covariates = list(X = X, Z = Z)
+
+  l <- list(y = results, theta = parameters, subject_ind = subject_ind,
+            time_ind = time_ind, covariates = covariates)
+
+  class(l) <- c("zibSim", "listof")
+
+  return(l)
+}
